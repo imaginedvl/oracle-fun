0ed5579fb1d8db630311065f5e299ffa5b48c2ff
imaginedvl <imaginedvl@users.noreply.github.com>
2021-07-18 18:51:44 -0400

    Local and Session support + Unit Tests

--- src/js/Oracle.Storage.js
+++ src/js/Oracle.Storage.js
@@ -8,7 +8,14 @@ Oracle = (function (parent) {
 
     const _result = parent.Storage;
 
-    _result.useMemory = typeof (Storage) === "undefined";
+    let _localStorage = null;
+    if (typeof (Storage) !== "undefined") {
+        _localStorage = window.localStorage;
+    }
+    else {
+        Oracle.Logger.logWarning("Local Storage not supported by this browser/environment, using memory instead");
+    }
+    _result.useMemory = _localStorage === null;
 
     // To make this compatible with everything, we will use "string" to store the data (cookie, storage, memory)
     // Then this module will have typed method
@@ -16,13 +23,12 @@ Oracle = (function (parent) {
     // 1 - We are limited by a simple key, value (where both are strings)
     // 2 - The value has a limit in size
 
-    // Those 2 methods can be overriden later
     _result.onReadLocalStringValueFromStorage = function (name) {
-        // To be overriden for now
-        return defaultValue;
+        return _localStorage.getItem(name);
     }
 
     _result.onWriteLocalStringValueToStorage = function (name, value) {
+        _localStorage.setItem(name, value);
     }
 
     const _localValueNameValidChars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_-.|';
@@ -38,10 +44,10 @@ Oracle = (function (parent) {
         JSON: 'J'
     }
 
-    const _normalizeLocalValueName = function (name) {
+    const _normalizeLocalValueName = function (context, name) {
         if (Oracle.isString(name) && !Oracle.isEmptyOrWhiteSpaces(name)) {
             // TODO: LD: normalize the string with the valid chars only and replace invalid chars with "_"
-            return _localStorageContext + "|" + name;
+            return context + "|" + name;
         }
         else {
             throw new Oracle.Errors.ValidationError("Local value name is either empty or invalid: '" + name + "'");
@@ -49,18 +55,8 @@ Oracle = (function (parent) {
     }
 
     const _memoryData = {};
-    let _localStorageContext = "Oracle";
-
-    _result.setLocalStorageContext = function (context) {
-        if (Oracle.isString(context) && !Oracle.isEmptyOrWhiteSpaces(context)) {
-
-        }
-        else {
-            _localStorageContext = "Oracle";
-        }
-    }
-    const _readLocalStringValueFromStorage = function (name) {
-        name = _normalizeLocalValueName(name);
+    const _readLocalStringValueFromStorage = function (context, name) {
+        name = _normalizeLocalValueName(context, name);
         if (_result.useMemory) {
             if (_memoryData.hasOwnProperty(name)) {
                 return _memoryData[name];
@@ -83,8 +79,8 @@ Oracle = (function (parent) {
         }
     }
 
-    const _writeLocalStringValueToStorage = function (name, value) {
-        name = _normalizeLocalValueName(name);
+    const _writeLocalStringValueToStorage = function (context, name, value) {
+        name = _normalizeLocalValueName(context, name);
         value = Oracle.toNullableValue(value);
         if (_result.useMemory) {
             _memoryData[name] = value;
@@ -98,68 +94,90 @@ Oracle = (function (parent) {
     }
 
     // ---------------------------------------------------------------------------------------------------------------- //
-    // Read / Write method for each supported type 
+    // Base Read/Write class
     // ---------------------------------------------------------------------------------------------------------------- //
+    const _baseStorageClass = class {
 
-    /* Object */
-    _result.readLocalValue = function (name, defaultValue = null) {
-        const storedValue = _readLocalStringValueFromStorage(name);
-        if (Oracle.isString(storedValue) && !Oracle.isEmptyOrWhiteSpaces(storedValue)) {
-            const char = storedValue.charAt(storedValue);
-            switch (char) {
-                case _localPrefixes.Undefined:
-                    return undefined;
-                case _localPrefixes.Null:
-                    return null;
-                case _localPrefixes.EmptyString:
-                    return "";
-                case _localPrefixes.String:
-                    return storedValue.substring(1);
-                case _localPrefixes.Number:
-                    return Number(storedValue.substring(1));
-                case _localPrefixes.Boolean:
-                    return Boolean(storedValue.substring(1));
-                case _localPrefixes.Date:
-                    return new Date(storedValue.substring(1));
-            }
+        constructor(settings) {
+            this.readCallback = settings.readCallback;
+            this.writeCallback = settings.writeCallback;
         }
-        else {
-            return defaultValue;
-        }
-    }
 
-    _result.writeLocalValue = function (name, value) {
-        let storedValue;
-        if (value === undefined) {
-            storedValue = _localPrefixes.Undefined;
-        }
-        else if (value === null) {
-            storedValue = _localPrefixes.Null;
-        }
-        else if (typeof (value) === 'number') {
-            storedValue = _localPrefixes.Number + value;
-        }
-        else if (typeof (value) === 'boolean') {
-            storedValue = _localPrefixes.Boolean + value;
-        }
-        else if (typeof (value) === 'string' || value instanceof String) {
-            if (value === '') {
-                storedValue = _localPrefixes.EmptyString;
+        readValue(path, name, defaultValue = null) {
+            const storedValue = this.readCallback(path, name);
+            if (Oracle.isString(storedValue) && !Oracle.isEmptyOrWhiteSpaces(storedValue)) {
+                const char = storedValue.charAt(storedValue);
+                switch (char) {
+                    case _localPrefixes.Undefined:
+                        return undefined;
+                    case _localPrefixes.Null:
+                        return null;
+                    case _localPrefixes.EmptyString:
+                        return "";
+                    case _localPrefixes.String:
+                        return storedValue.substring(1);
+                    case _localPrefixes.Number:
+                        return Number(storedValue.substring(1));
+                    case _localPrefixes.Boolean:
+                        return Boolean(storedValue.substring(1));
+                    case _localPrefixes.Date:
+                        return new Date(storedValue.substring(1));
+                    case _localPrefixes.JSON:
+                        return JSON.parse(storedValue.substring(1));
+                }
             }
             else {
-                storedValue = _localPrefixes.String + value;
+                return defaultValue;
             }
         }
-        else if (value instanceof Date) {
-            storedValue = _localPrefixes.Date + value.toISOString();
+
+        writeValue(path, name, value) {
+            let storedValue;
+            if (value === undefined) {
+                storedValue = _localPrefixes.Undefined;
+            }
+            else if (value === null) {
+                storedValue = _localPrefixes.Null;
+            }
+            else if (typeof (value) === 'number') {
+                storedValue = _localPrefixes.Number + value;
+            }
+            else if (typeof (value) === 'boolean') {
+                storedValue = _localPrefixes.Boolean + value;
+            }
+            else if (typeof (value) === 'string' || value instanceof String) {
+                if (value === '') {
+                    storedValue = _localPrefixes.EmptyString;
+                }
+                else {
+                    storedValue = _localPrefixes.String + value;
+                }
+            }
+            else if (value instanceof Date) {
+                storedValue = _localPrefixes.Date + value.toISOString();
+            }
+            else if (typeof (value) === 'object') {
+                storedValue = _localPrefixes.JSON + JSON.stringify(value);
+            }
+            else {
+                throw new Oracle.Errors.RuntimeError("Cannot store value because it is not supported.");
+            }
+            Oracle.Logger.logDebug("Storing local value.", { name: name, storedValue: storedValue });
+            this.writeCallback(path, name, storedValue);
         }
-        else {
-            throw new Oracle.Errors.RuntimeError("Cannot store value because it is not supported.");
-        }
-        Oracle.Logger.logDebug("Storing local value.", { name: name, storedValue: storedValue });
-        _writeLocalStringValueToStorage(name, storedValue);
     }
 
-    _result.setLocalStorageContext();
+    _result.Local = new _baseStorageClass(
+        {
+            readCallback: _readLocalStringValueFromStorage,
+            writeCallback: _writeLocalStringValueToStorage
+        });
+
+    _result.Session = new _baseStorageClass(
+        {
+            readCallback: (path, name) => { return sessionStorage[_normalizeLocalValueName(path, name)] },
+            writeCallback: (path, name, value) => { sessionStorage[_normalizeLocalValueName(path, name)] = value; }
+        });
+
     return parent;
 }(Oracle));
--- src/js/tests/Tests.Oracle.Storage.js
+++ src/js/tests/Tests.Oracle.Storage.js
@@ -1,60 +1,120 @@
 'use strict';
 
-Oracle.Tests.registerModule({
-    name: 'Oracle.Storage',
-    testInitialization: () => {
-        Oracle.Storage.useMemory = true;
-        Oracle.Storage.setLocalStorageContext("Tests.Oracle.Storage");
-    }
-})
+(function () {
 
-Oracle.Tests.registerTest({
-    module: 'Oracle.Storage',
-    category: 'Local Values',
-    name: 'Long Length Values',
-    test: (assert, logger) => {
-        Oracle.Storage.useMemory = true;
+    // Module registration if we want to execute something before each test...
+    Oracle.Tests.registerModule({
+        name: 'Oracle.Storage',
+        testInitialization: () => {
+        }
+    })
+
+
+    // We are going to test every storage (Local, Session, ...) using shared tests
+
+    const _longLengthStringsTest = function (storage, assert, logger) {
+        const valueName = "LongLengthValues"
         const c_10 = "0123456789";
         const c_100 = c_10 + c_10 + c_10 + c_10 + c_10 + c_10 + c_10 + c_10 + c_10 + c_10;
         const c_1000 = c_100 + c_100 + c_100 + c_100 + c_100 + c_100 + c_100 + c_100 + c_100 + c_100;
-        Oracle.Storage.setLocalStorageContext("Tests.Oracle.Storage");
-        Oracle.Storage.writeLocalValue("TestData", c_10);
-        assert.areEqual(c_10, Oracle.Storage.readLocalValue("TestData", null));
+        const c_10000 = c_1000 + c_1000 + c_1000 + c_1000 + c_1000 + c_1000 + c_1000 + c_1000 + c_1000 + c_1000;
+        storage.writeValue("Test.Oracle.Storage", valueName, c_10000);
+        assert.areEqual(c_10000, storage.readValue("Test.Oracle.Storage", valueName, null));
     }
-});
 
-Oracle.Tests.registerTest({
-    module: 'Oracle.Storage',
-    category: 'Local Values',
-    name: 'Basic Typed Values',
-    test: (assert, logger) => {
+    const _basicTypedValuesTest = function (storage, assert, logger) {
         const valueName = "BasicTypedValues"
         const currentDate = new Date();
-        Oracle.Storage.writeLocalValue(valueName, "This is a string");
-        assert.areStrictlyEqual("This is a string", Oracle.Storage.readLocalValue(valueName))
-        Oracle.Storage.writeLocalValue(valueName, true);
-        assert.areStrictlyEqual(true, Oracle.Storage.readLocalValue(valueName))
-        Oracle.Storage.writeLocalValue(valueName, 125.20);
-        assert.areStrictlyEqual(125.20, Oracle.Storage.readLocalValue(valueName))
-        Oracle.Storage.writeLocalValue(valueName, currentDate);
-        assert.areComparable(currentDate, Oracle.Storage.readLocalValue(valueName))
-        Oracle.Storage.writeLocalValue(valueName, null);
-        assert.isNull(Oracle.Storage.readLocalValue(valueName))
-        Oracle.Storage.writeLocalValue(valueName, undefined);
-        assert.isUndefined(Oracle.Storage.readLocalValue(valueName))
-        Oracle.Storage.writeLocalValue(valueName, "");
-        assert.areStrictlyEqual("", Oracle.Storage.readLocalValue(valueName))
+        storage.writeValue("Test.Oracle.Storage", valueName, "This is a string");
+        assert.areStrictlyEqual("This is a string", storage.readValue("Test.Oracle.Storage", valueName))
+        storage.writeValue("Test.Oracle.Storage", valueName, true);
+        assert.areStrictlyEqual(true, storage.readValue("Test.Oracle.Storage", valueName))
+        storage.writeValue("Test.Oracle.Storage", valueName, 125.20);
+        assert.areStrictlyEqual(125.20, storage.readValue("Test.Oracle.Storage", valueName))
+        storage.writeValue("Test.Oracle.Storage", valueName, currentDate);
+        assert.areComparable(currentDate, storage.readValue("Test.Oracle.Storage", valueName))
+        storage.writeValue("Test.Oracle.Storage", valueName, null);
+        assert.isNull(storage.readValue("Test.Oracle.Storage", valueName))
+        storage.writeValue("Test.Oracle.Storage", valueName, undefined);
+        assert.isUndefined(storage.readValue("Test.Oracle.Storage", valueName))
+        storage.writeValue("Test.Oracle.Storage", valueName, "");
+        assert.areStrictlyEqual("", storage.readValue("Test.Oracle.Storage", valueName))
     }
-});
 
-Oracle.Tests.registerTest({
-    module: 'Oracle.Storage',
-    category: 'Local Values',
-    name: 'Conversions',
-    test: (assert, logger) => {
-        Oracle.Storage.useMemory = true;
-
-        Oracle.Storage.read
-
+    const _complexValuesTest = function (storage, assert, logger) {
+        const valueName = "ComplexTypedValues"
+        const a = {
+            value: 'value',
+            bool: true,
+            number: 125.10,
+            date: new Date(),
+            sub:
+            {
+                id: 1,
+                name: 'Joel'
+            }
+        }
+        storage.writeValue("Test.Oracle.Storage", valueName, a);
     }
-});
+
+    // -----------------------------------------------------
+    // Local Storage 
+    // -----------------------------------------------------
+    Oracle.Tests.registerTest({
+        module: 'Oracle.Storage',
+        category: 'Local Storage',
+        name: 'Long Length Strings (10000 chars)',
+        test: (assert, logger) => {
+            _longLengthStringsTest(Oracle.Storage.Local, assert, logger);
+        }
+    });
+
+    Oracle.Tests.registerTest({
+        module: 'Oracle.Storage',
+        category: 'Local Storage',
+        name: 'Basic Typed Values',
+        test: (assert, logger) => {
+            _basicTypedValuesTest(Oracle.Storage.Local, assert, logger);
+        }
+    });
+
+    Oracle.Tests.registerTest({
+        module: 'Oracle.Storage',
+        category: 'Local Storage',
+        name: 'Complex Values',
+        test: (assert, logger) => {
+            _complexValuesTest(Oracle.Storage.Local, assert, logger);
+        }
+    });
+
+    // -----------------------------------------------------
+    // Session Storage 
+    // -----------------------------------------------------
+    Oracle.Tests.registerTest({
+        module: 'Oracle.Storage',
+        category: 'Session Storage',
+        name: 'Long Length Strings (10000 chars)',
+        test: (assert, logger) => {
+            _longLengthStringsTest(Oracle.Storage.Session, assert, logger);
+        }
+    });
+
+    Oracle.Tests.registerTest({
+        module: 'Oracle.Storage',
+        category: 'Session Storage',
+        name: 'Basic Typed Values',
+        test: (assert, logger) => {
+            _basicTypedValuesTest(Oracle.Storage.Session, assert, logger);
+        }
+    });
+
+    Oracle.Tests.registerTest({
+        module: 'Oracle.Storage',
+        category: 'Session Storage',
+        name: 'Complex Values',
+        test: (assert, logger) => {
+            _complexValuesTest(Oracle.Storage.Session, assert, logger);
+        }
+    });
+
+})();
