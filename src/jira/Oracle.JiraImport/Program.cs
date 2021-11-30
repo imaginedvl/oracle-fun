037e78ce51e80d870251fea174462d6ba4b84d87
SimonLaBarre <82888637+SimonLaBarre@users.noreply.github.com>
2021-11-30 15:00:43 -0500

    Project Skeleton
    Created:
    * Projects
    * NuGet packages
    * config.json

---
+++ src/jira/Oracle.JiraImport/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+
+namespace Oracle.JiraImport
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetParent(AppContext.BaseDirectory).FullName)
+                .AddJsonFile("config.json", false)
+                .Build();
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
