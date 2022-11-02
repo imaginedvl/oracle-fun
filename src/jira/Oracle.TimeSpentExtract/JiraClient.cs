f2de967578f558abf0048d89f61efd3f192a9cdf
SimonLaBarre <82888637+SimonLaBarre@users.noreply.github.com>
2022-11-02 15:36:17 -0400

    New Project TimeSpentExtract
    Add new Project for Time Tracking
    
    Refactoring a bit

---
+++ src/jira/Oracle.TimeSpentExtract/JiraClient.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Extensions.Configuration;
+using Model.Jira;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using System.Web;
+using System.Xml;
+
+namespace JiraImport
+{
+    public class JiraClient
+    {
+
+        private static readonly string URL = "https://{0}/jira/rest/api/latest";
+
+        private readonly string _host;
+        private readonly string _username;
+        private readonly string _password;
+
+        public JiraClient(IConfigurationRoot configuration)
+        {
+            _host = configuration.GetSection("Environment").Value;
+            Console.WriteLine("[Using Environment]: {0}", _host);
+            _username = configuration.GetSection("User").GetSection("Username").Value;
+            _password = configuration.GetSection("User").GetSection("Password").Value;
+
+            CheckStatus().Wait();
+        }
+
+        private HttpClient CreateClient()
+        {
+            HttpClient client = new()
+            {
+                Timeout = TimeSpan.FromSeconds(120)
+            };
+            client.DefaultRequestHeaders.Accept.Clear();
+
+            string encodedCredentials = Convert.ToBase64String(Encoding.UTF8.GetBytes(string.Format("{0}:{1}", _username, _password)));
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", encodedCredentials);
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            return client;
+        }
+
+        private async Task CheckStatus()
+        {
+            HttpClient client = CreateClient();
+            string url = string.Format("https://{0}/jira/status", _host);
+            Console.Write("[Checking Environment Status]: ");
+            string stringTask = await client.GetStringAsync(url);
+            JsonNode? o = JsonNode.Parse(stringTask!);
+            if (o != null)
+                Console.WriteLine(o!["state"]);
+            else
+                throw new JsonException(string.Format("Could not parse url GET:\n{0}\n{1}", url, stringTask));
+        }
+    }
+}
---
+++ src/jira/Oracle.TimeSpentExtract/Program.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace JiraImport
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Need to specify the Excel file to import from.");
+                return;
+            }
+
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetParent(AppContext.BaseDirectory)!.FullName)
+                .AddJsonFile("config.json", false)
+                .Build();
+
+            try
+            {
+                Console.WriteLine("Initialization...");
+                JiraClient jiraClient = new(configuration);
+
+                // Fetch the Epics and Stories and Sub-Tasks using the JQL
+                // Sum up the time spent
+
+                Console.WriteLine("Done! Press a key to quit.");
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
